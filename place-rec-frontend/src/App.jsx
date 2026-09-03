0a8d47729e3d50d8900fd4690615838ce4a5f7f7
Siti Nahlah Salsabila <151691753+bedaddict@users.noreply.github.com>
2026-09-03 15:07:47 +0700

    Set up unified Vercel deployment

--- place-rec-frontend/src/App.jsx
+++ place-rec-frontend/src/App.jsx
@@ -76,7 +76,7 @@ function App() {
 
     try {
       // Notice the new &shuffle parameter at the end of the URL!
-      const response = await fetch(`http://127.0.0.1:8000/api/itinerary?location=${encodeURIComponent(searchQuery)}&shuffle=${isShuffle}`);
+      const response = await fetch(`/api/itinerary`);
 
       const data = await response.json();
 
